20addc3f9cefe786923e93acee9ec307d94852ac
shamoon1997 <sshamoon83@gmail.com>
2022-08-10 15:48:13 +0500

    difference between push and concat handle

---
+++ index1.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3];
+
+console.log(arr.push(5));
+
+console.log('After pushing element to array', arr);
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = arr1.concat(arr2);
+
+console.log(arr3);
